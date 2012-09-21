e0c43194af2ac646fd4b7018ab3eceaf5c7c87dc
Beatlepl <beatlepl@gmail.com>
2012-09-21 13:02:15 -0700

    Initial drop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerOsxDemo::Application.config.secret_token = 'eb9e5246179baccf419b713ae6d7e758fb78ef41368199925c4fb63f5b032d46556bf893c6dd906ba9f7651082f6274ec896a18130067b06de5b9392f665020b'
